13375ef01d59a85cf12af22edd063a55049b9f0c
Melvin Vermeer <Melvin.Vermeer@wortell.nl>
2018-03-16 15:44:52 +0100

    Added comments about expected webhook payload

--- runbooks/createfolder.ps1
+++ runbooks/createfolder.ps1
@@ -4,3 +4,9 @@ $Data = ConvertFrom-Json -inputobject $WebhookData.RequestBody
 $Folder = "C:\Temp\" + $Data.FolderName
 
 New-Item -ItemType directory -Path $Folder
+
+# Send HTTTP POST request to the webhook endpoint with the following JSON payload
+#
+# {
+# 	"FolderName" : "SampleFolder"
+# }
--- runbooks/newuser.ps1
+++ runbooks/newuser.ps1
@@ -9,3 +9,12 @@ New-MsolUser -DisplayName $user.DisplayName `
 -FirstName $user.FirstName `
 -LastName $user.LastName `
 -UserPrincipalName $user.UserPrincipalName
+
+# Send HTTTP POST request to the webhook endpoint with the following JSON payload
+#
+# {
+# 	"DisplayName" : "SampleUser",
+# 	"FirstName" : "Sample",
+# 	"LastName" : "User",
+# 	"UserPrincipalName" : "Sample.User@yourdomain.onmicrosoft.com"
+# }
