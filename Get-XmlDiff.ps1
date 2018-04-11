b8c773b3a8fabe4a4c1eae68d7f29d90719181eb
Melvin Vermeer <Melvin.Vermeer@wortell.nl>
2018-04-11 09:49:32 +0200

    Get-XmlDiff now accepts two xml structures and returns a single stucture with the diff

--- Get-XmlDiff.ps1
+++ Get-XmlDiff.ps1
@@ -1,26 +1,30 @@
-function Get-XmlDiff($actual, $expected, $node) {
+function Get-XmlDiff($actual, $expected) {
+    $doc = New-Object System.Xml.XmlDocument
+    $root = $doc.CreateNode("element", "Root", $null)
+    
+    Get-XmlDiffRecursive $old.Configuration $new.Configuration $root
+    
+    $doc.AppendChild($root) | Out-Null
+    return $doc.Root.InnerXml
+}
+
+function Get-XmlDiffRecursive($actual, $expected, $node) {
     if ($actual.OuterXml -ne $expected.OuterXml) {
-        if($expected.NodeType -eq "Element") {
+        if ($expected.NodeType -eq "Element") {
             $element = $doc.CreateElement($expected.Name)
             $node.AppendChild($element) | Out-Null
-        }else{
+        }
+        else {
             $node.InnerText = $expected.OuterXml
         }
     }
   
     for ($i = 0; $i -lt $expected.ChildNodes.Count; $i = $i + 1) {
-        Get-XmlDiff $actual.ChildNodes[$i] $expected.ChildNodes[$i] $element
+        Get-XmlDiffRecursive $actual.ChildNodes[$i] $expected.ChildNodes[$i] $element
     }
 }
 
-$doc = New-Object System.Xml.XmlDocument
-
 [xml]$old = Get-Content "configuration.old.xml"
 [xml]$new = Get-Content "configuration.new.xml"
-
-$root = $doc.CreateNode("element", "Root", $null)
-
-Get-XmlDiff $old.Configuration $new.Configuration $root
-
-$doc.AppendChild($root) | Out-Null
-$doc.Root.InnerXml | Out-File new-config.xml -encoding "UTF8"
+cls
+Get-XmlDiff $old $new | Out-File new-config.xml -encoding "UTF8"
